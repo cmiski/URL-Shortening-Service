eee8ac5434052a8c8f8d0949006e0bca5a7acc95
cmiski <oshorwtt11@gmail.com>
2026-01-22 23:24:08 +0530

    added url model

---
+++ src/models/url.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortCode: {
+      type: String,
+      required: true,
+      unique: true, // guarantees no two URLs collide
+      index: true, // faster lookup to accomodate traffic
+    },
+    longUrl: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    expiresAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Url", urlSchema);
